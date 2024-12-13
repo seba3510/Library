2dcf0456e7e1222f63393aa083cc78bf5df1f604
seba3510 <seba3510@protonmail.com>
2024-12-13 11:41:15 -0400

    Add contents of form

--- scripts/library.js
+++ scripts/library.js
@@ -1,6 +1,11 @@
 // Stores all of the books
 const books = [];
 
+/**
+ * Reference to the button that is used to add a new book.
+ * 
+ * @type {HTMLElement}
+ */
 const button = document.getElementById("add");
 
 /**
@@ -10,21 +15,23 @@ const button = document.getElementById("add");
  * @param {String} author - The author of the book.
  * @param {Number} pages - The total number of pages that the book has.
  * @param {Boolean} read - Determines if the user has read the book or not.
+ * @param {Number} price - The price for which the book was bought.
  */
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, price) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.price = price;
 }// Book()
 
 //==================================================================================
 
-function handleClick() {
+function addBook() {
     button.addEventListener("click", () => {
         let msg = "You have clicked me!";
         alert(msg);
     });
 }//handleClick()
 
-handleClick();
+addBook();
