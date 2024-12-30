1a5023d45871da8ec5d6c2c02f2623d4778aa5cb
seba3510 <seba3510@protonmail.com>
2024-12-30 14:09:41 -0400

    feature: add events to display modal dialog
    Implement an event listener to show the form on click

--- library.js
+++ library.js
@@ -1,6 +1,34 @@
+const library = [];
+
 const dialog = document.querySelector("#dialog");
 const openButton = document.querySelector("#open-modal");
 const closeButton = document.querySelector("#close-modal");
+const form = document.querySelector("form");
+
+const title = document.querySelector("#book-title");
+const author = document.querySelector("#book-author");
+const numPages = document.querySelector("#num-pages");
+// const hasRead = document.querySelector("");
+
+//========================================================================
+
+function Book(author, title, pages, hasRead) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}//Book()
+//========================================================================
+
+//========================================================================
+
+function addBookToLibrary(author, title, numPages) {
+    let newBook = new Book(author.value, title.value, numPages.value);
+    library.push(newBook);
+}//addBookToLibrary()
+
+//========================================================================
+
 
 // "Show the dialog" button opens the dialog modally
 openButton.addEventListener("click", () => {
@@ -11,3 +39,25 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     dialog.close();
 });
+
+
+function handleClick() {
+    let n = library.length;
+
+    for (let i = 0; i < n; i++) {
+        displayForm();
+        validateForm();
+    }//for()
+
+} // handleClick()
+//========================================================================
+
+function displayForm() {
+    openButton.addEventListener("click", (event) => {
+        dialog.showModal();
+    });
+}//displayForm()
+//========================================================================
+
+
+handleClick();
