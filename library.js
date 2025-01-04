2f73555161d1b9130f994c9db89438342cd190ee
seba3510 <seba3510@protonmail.com>
2025-01-04 09:55:55 -0400

    feature: add css classes for validation of input fields

--- library.js
+++ library.js
@@ -1,78 +1,8 @@
-const library = [];
-
-const dialog = document.querySelector("#dialog");
-const openButton = document.querySelector("#open-modal");
-const closeButton = document.querySelector("#close-modal");
-const form = document.querySelector("form");
-
-const title = document.querySelector("#book-title");
-const author = document.querySelector("#book-author");
-const numPages = document.querySelector("#num-pages");
-// const hasRead = document.querySelector("");
-
-//========================================================================
-
-function Book(author, title, pages, hasRead) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.hasRead = hasRead;
-}//Book()
-//========================================================================
-
-//========================================================================
-
-function addBookToLibrary(author, title, numPages) {
-    let newBook = new Book(author.value, title.value, numPages.value);
-    library.push(newBook);
-}//addBookToLibrary()
-
-//========================================================================
-
-
-// "Show the dialog" button opens the dialog modally
-openButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-    dialog.close();
-});
-
-
-function handleClick() {
-    let n = library.length;
-
-    for (let i = 0; i < n; i++) {
-        displayForm();
-        validateForm();
-    }//for()
-
-} // handleClick()
-//========================================================================
-
-function displayForm() {
-    openButton.addEventListener("click", (event) => {
-        dialog.showModal();
-    });
-}//displayForm()
-
-//========================================================================
-
-function validateForm() {
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        validate
-    });
-}//validateForm()
-
-//========================================================================
-
-function isEmpty(str) {
-    return ((str.trim().length === 0));
-}//str()
-
-//========================================================================
-
-handleClick();
+const library = [
+    {
+        "title": "The Hunger Games",
+        "author": "Suzanne Collins",
+        "numPages": 400,
+        "read": true
+    }
+]
