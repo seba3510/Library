a4facbebbd00edd3e66911276f46676a8474f037
seba3510 <seba3510@protonmail.com>
2025-01-04 10:15:48 -0400

    feat: Create array of books and constructor

--- library.js
+++ library.js
@@ -1,8 +1,51 @@
-const library = [
+const form = document.querySelector("form");
+
+const titleElem = document.querySelector("#book-title");
+const authorElem = document.querySelector("#book-author");
+const numPagesElem = document.querySelector("#num-pages");
+
+//=======================================================================================================
+
+/**
+ * Array of objects that stores all books of the library.
+ * 
+ * @type {Book}
+ * 
+ * @note I manually added a few books to the array so I can see the display
+ */
+let library = [
     {
         "title": "The Hunger Games",
         "author": "Suzanne Collins",
-        "numPages": 400,
+        "pages": 400,
         "read": true
+    },
+
+    {
+        "title": "Harry Potter and the Sorcerer's Stone",
+        "author": "J.K. Rowling",
+        "pages": 320,
+        "read": false
+    },
+
+    {
+        "title": "A Tale of Two Cities",
+        "author": "Charles Dickens",
+        "pages": 428,
+        "read": false
     }
-]
+];
+
+//=======================================================================================================
+
+/**
+ * Creates a new Book object 
+ * 
+ * @param {String} title  The title of the book 
+ * @param {*} author  The author of the book
+ * @param {*} pages  The total number of pages that the book has
+ * @param {*} read   Status that indicates whether the book has been read, or not
+ */
+function Book(title, author, pages, read) {
+
+}//Book()
