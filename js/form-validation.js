b26c8f8f1f2fc030733c0e531a56ed8ad969e84e
seba3510 <seba3510@protonmail.com>
2025-01-11 13:44:31 -0400

    docs: update documentation

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,82 +1,34 @@
-/**
- * This script validates the form in which the user enters a
- * book to store in the library.
- * 
- * @author Sebastian Corporan Berrios
- */
 
 
-/**
- * Reference to the form in which the user will add a book to the library.
- * 
- * @type {HTMLElement}
- */
 const form = document.querySelector("form");
 
-/**
- * Reference to the input field that stores the title of the book.
- * 
- * @type {HTMLElement}
- */
+
 const titleElem = document.querySelector("#book-title");
 
-/**
- * Reference to the input field that stores the author of the book.
- * 
- * @type {HTMLElement}
- */
+
 const authorElem = document.querySelector("#book-author");
 
-/**
- * Reference to the input field that stores the number of pages that the book has.
- * 
- * @type {HTMLElement}
- */
+
 const numPagesElem = document.querySelector("#num-pages");
 
-/**
- * Reference to the input field that indicates whether the book has been read.
- * This element represents the user's selection confirming that the book has been read.
- * 
- * @type {HTMLElement}
- */
+
 const haveRead = document.querySelector("#agree");
 
-/**
- * Reference to the input field that indicates  that the book has not been been read.
- * This element represents the user's selection confirming the the book has not been read.
- */
+
 const haveNotRead = document.querySelector("#disagree");
 
-/**
- * Reference to the dialog element that displays the form.
- * 
- * @type {HTMLElement}
- */
 const dialogElem = document.querySelector("#dialog");
 
-/**
- * Assigns an id to each book.
- * This was done to facilitate the implementation of removing 
- * a book from the library, and toggling the book status.
- * 
- * This id starts from 0 to link via data-attribute the index of the book in the array
- * in which will be deleted.
- */
+
 let bookId = Number(0);
 //=======================================================================================================
 
-/**
- * Displays the form when the user clicks wants to add a new book to the library.
- */
 function displayForm() {
 
-	// Select the button element with the ID "open-modal"
+
 	const btn = document.querySelector("#open-modal");
 
-	// Add an event listener to the button that listens for the "click" event
 	btn.addEventListener("click", () => {
-		// Show the modal dialog when the button is clicked
 		dialogElem.showModal();
 	});
 
@@ -84,9 +36,6 @@ function displayForm() {
 
 //=======================================================================================================
 
-/**
- * Closes the form.
- */
 function closeForm() {
 
 	const btn = document.querySelector("#close-modal");
@@ -98,11 +47,7 @@ function closeForm() {
 
 //=======================================================================================================
 
-/**
- * Handles the button click by calling displayForm.
- * This function is a wrapper around the `displayForm` function. It can be used to
- * trigger the display of the form/modal, likely in response to some other logic or user interaction.
- */
+
 function handleClick() {
 	displayForm();
 	submitForm();
@@ -110,17 +55,15 @@ function handleClick() {
 } // handleClick()
 //=======================================================================================================
 
-/**
- * Submits the form filled out by the user, so the book can be added to the library, and displayed
- */
+
 function submitForm() {
 
 	form.addEventListener("submit", (event) => {
 
-		// Prevents the form from submitting
+
 		event.preventDefault();
 
-		// Validate input fields
+
 		checkTitle();
 		checkAuthor();
 		checkNumberOfPages();
@@ -139,12 +82,7 @@ function submitForm() {
 
 //=======================================================================================================
 
-/**
- * Displays the specified error message for the given input field.
- * 
- * @param {HTMLElement} input - The input field element that is being validated
- * @param {string} message - The error message to display next to the input field
- */
+
 function showError(input, message) {
 
 	const formField = input.parentElement;
@@ -157,12 +95,7 @@ function showError(input, message) {
 
 //=======================================================================================================
 
-/**
- * Updates the class of the parent element of the provided input field
- * to indicate a successful form input.
- * 
- * @param {HTMLElement} input - The input field whose parent element will be updated.
- */
+
 function showSuccess(input) {
 
 	const formField = input.parentElement;
@@ -177,28 +110,14 @@ function showSuccess(input) {
 
 //=======================================================================================================
 
-/**
- * Determines whether the specified string is empty.
- * 
- * @param {String} str - The specified string.
- * 
- * @returns {Boolean} True if the string is empty. False otherwise
- */
+
 function isEmpty(str) {
 	return ((str === ""));
 }// isEmpty()
 
 //=======================================================================================================
 
-/**
- * Determines if the specified value is between the specified range.
- * 
- * @param {Number} val - The specified value.
- * @param {Number} min - The lower bound of the range.
- * @param {Number} max - The upper bound of the range.
- * 
- * @returns {Boolean}  True if the value if between the specified range. False otherwise.
- */
+
 function isBetween(val, min, max) {
 
 	let isBetween = false;
@@ -219,11 +138,7 @@ function isBetween(val, min, max) {
 
 //=======================================================================================================
 
-/**
- * Validates the status of the book.
- * 
- * @returns {String} True  if the book has been read.  False if the book has not been read
- */
+
 function checkStatus() {
 
 	let readStatus = false;
@@ -253,17 +168,10 @@ function checkStatus() {
 //=======================================================================================================
 
 
-/**
- * Validates the title of the book entered by the user.
- */
+
 function checkTitle() {
 
-	/**
-	 * Get the title entered by the user in the form.
-	 * 
-	 * @Note - The method .trim() was used to eliminate any white spaces
-	 * entered by the user.
-	 */
+
 	const titleValue = titleElem.value.trim();
 
 	if ((isEmpty(titleValue))) {
@@ -280,9 +188,6 @@ function checkTitle() {
 
 //=======================================================================================================
 
-/**
- * Validates the input value for the book author.
- */
 function checkAuthor() {
 
 	const authorValue = authorElem.value.trim();
@@ -315,9 +220,7 @@ function checkAuthor() {
 
 //=======================================================================================================
 
-/**
- * Validates the input value for the number of pages of the book.
- */
+
 function checkNumberOfPages() {
 
 	const numPagesValue = numPagesElem.value.trim();
@@ -340,9 +243,6 @@ function checkNumberOfPages() {
 
 //=======================================================================================================
 
-/**
- * Clears the input values entered by the user when the form is submitted.
- */
 function clearInputs() {
 
 	titleElem.value = "";
@@ -355,6 +255,4 @@ function clearInputs() {
 
 //=======================================================================================================
 
-
-// Trigger the 'click' event once the user clicks on the "Add New Book" button.
 handleClick();
--- js/library.js
+++ js/library.js
@@ -1,29 +1,12 @@
-/**
- * This script implements the library to store all the books
- * once the form is validated.
- * 
- * @author Sebastian Corporan Berrios
- */
 
 
-/**
- * Array of objects used to store each book
- * 
- * @type {Book}
- */
+
+
 const library = [];
 
 //=======================================================================================================
 
-/**
- * Creates a new Book object 
- * 
- * @param {Number} id - The id assigned to the book
- * @param {String} title - The title of the book 
- * @param {String} author - The author of the book
- * @param {Number} pages - The total number of pages that the book has
- * @param {Boolean} read - Status that indicates whether the book has been read, or not
- */
+
 function Book(id, title, author, pages, read) {
 	this.id = id;
 	this.title = title;
@@ -35,15 +18,6 @@ function Book(id, title, author, pages, read) {
 
 //=======================================================================================================
 
-/**
- * Adds the specified book to the library
- * 
- * @param {Number} id - The id of the book
- * @param {String} title - The title of the book    
- * @param {String} author - The author of the book
- * @param {Number} pages - The total number of pages that the book has
- * @param {Boolean} read - Status that indicates if the book has been read, or not
- */
 function addBookToLibrary(id, title, author, pages, read) {
 	let newBook = new Book(id, title, author, pages, read);
 	library.push(newBook);
@@ -53,52 +27,58 @@ function addBookToLibrary(id, title, author, pages, read) {
 //=======================================================================================================
 
 
-/**
- * Displays  the list of books in the library onto a table
-*/
+
 function displayLibrary() {
 
-	const tableContainer = document.querySelector("#table-container");
-	// Select the table element from the DOM
-	const table = document.querySelector("table");
+	const tableContainer = document.createElement("div");
+	tableContainer.setAttribute("id", "table-container");
 
-	// Make the table visible by setting its visibility style to 'visible'
-	table.style.visibility = "visible";
 
-	// Select the table body element where rows will be added
-	const tableBody = document.querySelector("tbody");
+	const table = document.createElement("table");
+	const tableHeader = document.createElement("thead");
+	const tableBody = document.createElement("tbody");
 
 
 
 
-	// Iterate over each book in the 'library' array
-	library.forEach(book => {
+	table.style.visibility = "visible"
 
-		// Create a new row element  for each book
-		const row = document.createElement("tr");
-
-		// Create a  cell for the id of the book, and set its text content to the id of book
-		const bookIdCell = document.createElement("td");
-		bookIdCell.textContent = `${book.id}`;
-		bookIdCell.style.visibility = "hidden";
+	library.forEach((book => {
 
 
-		// Create a new cell  for the book's title and set its text content to the book's title
+		const headerRow = document.createElement("tr");
+
+
+		const bookIDHeaderCell = document.createElement("th");
+		bookIDHeaderCell.textContent = "ID";
+
+		const titleHeaderCell = document.createElement("th");
+		titleHeaderCell.textContent = "Title";
+
+		const authorHeaderCell = document.createElement("th");
+		authorHeaderCell.textContent = "Author";
+
+		const pagesHeaderCell = document.createElement("th");
+		pagesHeaderCell.textContent = "Number of Pages";
+		// console.log(library);
+		const statusHeaderCell = document.createElement("th");
+		statusHeaderCell.textContent = "Has the book been read?";
+
+		const contentRow = document.createElement("tr");
+
+		const bookIDCell = document.createElement("td");
+		bookIDCell.textContent = `${book.id}`;
+
 		const titleCell = document.createElement("td");
 		titleCell.textContent = book.title;
 
-		// Create a new cell for the book's author and set its text content to the book's author
 		const authorCell = document.createElement("td");
 		authorCell.textContent = book.author;
 
-		// Create a new cell for the number of pages and set its text content
 		const pagesCell = document.createElement("td");
-		pagesCell.textContent = book.pages;
+		pagesCell.textContent = `${book.pages}`;
 
-
-		// Create a new cell for the book's reading status and set its text content
 		const statusCell = document.createElement("td");
-		// const statusCell = document.createElement("td");
 		statusCell.textContent = book.read;
 
 		const div = document.createElement("div");
@@ -108,9 +88,6 @@ function displayLibrary() {
 		deleteBtn.setAttribute("class", "remove-btn");
 		deleteBtn.setAttribute("data-bookId", `${book.id}`);
 
-
-
-		// Create an image element for the delete icon inside the button
 		const deleteIcon = document.createElement("img");
 
 		let path = "../images/delete.png";
@@ -118,49 +95,34 @@ function displayLibrary() {
 
 		deleteIcon.setAttribute("class", "remove-icon");
 
-
-		// Append the delete icon to the delete button
 		deleteBtn.appendChild(deleteIcon);
-
 		div.appendChild(deleteBtn);
 
-		// Append the delete button to the button cell
-		// deleteBtnCell.appendChild(deleteBtn);
 
-
-
-		// Crate the cell in which the button to change the read status will be displayed
-		// const changeStatusBtnCell = document.createElement("td");
-
-
-		// Crete the button that is used to toggle the read status of the book
 		const changeStatusBtn = document.createElement("button");
 		changeStatusBtn.setAttribute("data-bookId", `${book.id}`);
 		changeStatusBtn.textContent = "Change Status";
 		div.appendChild(changeStatusBtn);
 
 
-		tableContainer.appendChild(div);
-		// tableContainer.appendChild(div);
 
 
-		// Append the individual cells (title, author, pages, status, delete button) to  the row
-		// row.appendChild(bookIdCell);
-		row.appendChild(titleCell);
-		row.appendChild(authorCell);
-		row.appendChild(pagesCell);
-		row.appendChild(statusCell);
-		// row.appendChild(deleteBtnCell);
-		// row.appendChild(changeStatusBtnCell);
 
-		tableBody.appendChild(row);
+		contentRow.appendChild(bookIDCell);
+		contentRow.appendChild(titleCell);
+		contentRow.appendChild(authorCell);
+		contentRow.appendChild(pagesCell);
+		contentRow.appendChild(statusCell);
 
-		// Append the newly created row to the table body, making it part of the visible table
-		// tableBody.appendChild(div);
 
-		removeBookFromLibrary(deleteBtn, row);
+
+		tableBody.appendChild(contentRow);
+
+
+
+		removeBookFromLibrary(deleteBtn, contentRow);
 		toggleBookStatus(changeStatusBtn, statusCell, book.read);
-	});
+	}));
 
 	clearInputs();
 
@@ -169,29 +131,15 @@ function displayLibrary() {
 
 //=======================================================================================================
 
-/**
- * Removes the specified row from the library 
- * 
- * @param {HTMLElement} button - The button that performs the action of deleting the book from the library
- * @param {HTMLElement} row  - The row in the display of the library that contains  the book to be removed
- */
 function removeBookFromLibrary(button, row) {
 	button.addEventListener("click", () => {
 		row.remove(); // remove the book from the display
 		delete library[Number(button.getAttribute("data-bookId"))]; // remove the book from the 'library' array
-		return;
 	});
 }// removeBookFromLibrary()
 
 //=======================================================================================================
 
-/**
- * Clears the contents of the table. 
- * 
- * @Note - This function was added to fix an issue where 
- * every time a new book (except the first one) was added, {@link displayLibrary()}
- * would display both the already added books and the new one.
- */
 function clearTable() {
 	const tableBody = document.querySelector("tbody");
 	tableBody.innerHTML = "";
@@ -199,13 +147,6 @@ function clearTable() {
 
 //=======================================================================================================
 
-/**
- * Toggles the display status of the book
- * 
- * @param {@HTMLElement} button - The button that triggers the event to change the status
- * @param {@HTMLElement} cell - The cell in which the status will be displayed
- * @param {Boolean} status - The value of the book's read status
- */
 function toggleBookStatus(button, cell, status) {
 
 	button.addEventListener("click", (event) => {
