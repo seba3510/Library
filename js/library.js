9843d6170468882d67a072a3b99e969f6295d2ff
Seba3510 <seba3510@protonmail.com>
2025-04-19 17:24:49 -0400

    refactor: refactor code of library.js file

--- js/library.js
+++ js/library.js
@@ -1,200 +1,384 @@
-const library = [];
+	const library = 
+		[];
 
-//=======================================================================================================
+	//=======================================================================================================
 
-function Book(id, title, author, pages, read) {
-	this.id = id;
-	this.title = title;
-	this.author = author;
-	this.pages = formatNumber(pages);
-	this.read = read;
-}// Book()
+	function Book
+		(
+			id, 
+			title, 
+			author, 
+			pages, 
+			read
+		) {
+
+			this.id = 
+				id;
+
+			this.title = 
+				title;
+
+			this.author = 
+				author;
+
+			this.pages = 
+				formatNumber(pages);
+
+			this.read = 
+				read;
+
+	} // Book()
+
+	//=======================================================================================================
+
+	function addBookToLibrary
+		(
+			id, 
+			title, 
+			author, 
+			pages, 
+			read
+		) {
+
+			const  newBook = 
+				new Book
+					(
+						id, 
+						title, 
+						author, 
+						pages, 
+						read
+					);
+
+			library.push(newBook);
+
+			bookId++;
+
+		} // addBookToLibrary()
+
+	//=======================================================================================================
+
+	function displayLibrary() {
+
+		const body =
+			document.querySelector("body");
+
+		let tableContainer = 
+			document.querySelector("#table-container");
+
+		const doesTableContainerExist =
+			doesElementExist(tableContainer);
+
+		if (!doesTableContainerExist) {
+
+			tableContainer = 
+				document.createElement("div");
+
+			tableContainer.setAttribute("id", "table-container");
+
+			body.appendChild(tableContainer);
+
+		} // if
+
+		else {
+
+			clearTable();
+
+		} // else
+
+		const table = 
+			document.createElement("table");
+
+		const tableHeader = 
+			document.createElement("thead");
+
+		const tableBody = 
+			document.createElement("tbody");
+
+		table.appendChild(tableHeader);
+
+		table.appendChild(tableBody);
+
+		tableContainer.appendChild(table);
+
+		table.style.visibility = 
+			"visible";
+
+		const headerRow = 
+			document.createElement("tr");
+
+		const bookIDHeaderCell = 
+			document.createElement("th");
+
+		bookIDHeaderCell.textContent = 
+			"ID";
+
+		bookIDHeaderCell.style.visibility = 
+			"hidden";
+
+		const titleHeaderCell = 
+			document.createElement("th");
+
+		titleHeaderCell.textContent = 
+			"Title";
+
+		headerRow.appendChild(titleHeaderCell);
+
+		const authorHeaderCell = 
+		document.createElement("th");
+
+		authorHeaderCell.textContent = 
+			"Author";
+
+		headerRow.appendChild(authorHeaderCell);
+
+		const pagesHeaderCell = 
+			document.createElement("th");
+
+		pagesHeaderCell.textContent = 
+			"Number of Pages";
+
+		headerRow.appendChild(pagesHeaderCell);
+
+		const statusHeaderCell = 
+			document.createElement("th");
+
+		statusHeaderCell.textContent = 
+			"Has the book been read?";
+
+		headerRow.appendChild(statusHeaderCell);
+
+		tableHeader.appendChild(headerRow);
+
+		const btnsContainer = 
+			document.createElement("div");
+
+		btnsContainer.setAttribute("class", "btns-container");
+
+		tableContainer.appendChild(btnsContainer);
+
+		library.forEach((book => {
+
+			const contentRow = 
+				document.createElement("tr");
+
+			const bookIDCell = 
+				document.createElement("td");
+
+			bookIDCell.textContent = 
+				`${book.id}`;
+
+			bookIDCell.style.visibility = 
+				"hidden";
+
+			const titleCell = 
+				document.createElement("td");
+
+			titleCell.textContent = 
+				book.title;
+
+			contentRow.appendChild(titleCell);
+
+			const authorCell = 
+				document.createElement("td");
+
+			authorCell.textContent = 
+				book.author;
+
+			contentRow.appendChild(authorCell);
+
+			const pagesCell = 
+				document.createElement("td");
+
+			pagesCell.textContent = 
+				`${book.pages}`;
+				
+				contentRow.appendChild(pagesCell);
+
+			const statusCell = 
+				document.createElement("td");
+
+			statusCell.textContent = 
+				book.read;
+
+			contentRow.appendChild(statusCell);
+
+			tableBody.appendChild(contentRow);
+
+			const div = 
+				document.createElement("div");
+
+			div.setAttribute("id", "edit-btns-container");
+
+			const deleteBtn = 
+				document.createElement("button");
+
+			deleteBtn.setAttribute("class", "remove-btn");
+
+			deleteBtn.setAttribute("data-bookID", `${book.id}`);
+
+			const deleteIcon = 
+				document.createElement("img");
+
+			const path = 
+				"../images/delete.png";
+
+			deleteIcon.setAttribute("src", path);
+
+			deleteIcon.setAttribute("class", "remove-icon")
+
+			deleteBtn.appendChild(deleteIcon);
+
+			div.appendChild(deleteBtn);
+
+			const changeStatusBtn = 
+				document.createElement("button");
+
+			changeStatusBtn.setAttribute("data-bookID", `${book.id}`);
+				
+			changeStatusBtn.textContent = 
+					"Change Status";
+
+			div.appendChild(changeStatusBtn);
+
+			btnsContainer.appendChild(div);
+
+			clearInputs();
+
+			removeBookFromLibrary
+				(
+					deleteBtn, 
+					contentRow
+				);
+
+			toggleBookStatus
+				(
+					changeStatusBtn, 
+					statusCell, 
+					book.read
+				);
+
+		})); // foreach
+
+	} // displayLibrary()
+
+	//=======================================================================================================
+
+	function removeBookFromLibrary(button, row) {
+
+		button.addEventListener("click", () => {
+
+			// Removes the book from the table display
+			row.remove();
+
+			const index = 
+				Number
+					(
+						button.getAttribute("data-bookID")
+					);
+
+			// Remove the book from the array
+			delete library[index]; 
+
+			library.length--;
+		});
+
+	} // removeBookFromLibrary()
+
+	//=======================================================================================================
+
+	function clearTable() {
+
+		const tableContainer = 
+			document.querySelector("#table-container");
+
+		tableContainer.innerHTML = 
+			"";
+		
+	} // clearTable()
+
+	//=======================================================================================================
+
+	function toggleBookStatus
+		(
+			button, 
+			cell, 
+			status
+		) {
+
+			button.addEventListener("click", () => {
 
 
-//=======================================================================================================
+				switch (status) {
+
+					case true:
+
+						status =
+							false;
+
+						break;
+
+					case false:
+
+						status =
+							true;
+						
+						break;
+				
+					default:
+
+						break;
+
+				} // switch			
+
+				const index = 
+					Number
+						(
+							button.getAttribute("data-bookID")
+						);
+
+				library[index].read = 
+					status
+
+				cell.textContent = 
+					status;
+
+			});
+
+	} // toggleBookStatus();
+
+	//=======================================================================================================
 
 
-function addBookToLibrary(id, title, author, pages, read) {
-	let newBook = new Book(id, title, author, pages, read);
-	library.push(newBook);
-	bookId++;
-}// addBookToLibrary()
+	function doesElementExist
+		(
+			element
+		) {
 
-//=======================================================================================================
+			const doesExist =
+				element != null;
 
-function displayLibrary() {
+			const result =
+				(!doesExist)
+					? false
+					: true;
 
-	const body = document.querySelector("body");
+			return result;
+		
+	} // doesElementExist()
 
-	let tableContainer = document.querySelector("#table-container");
+	//=======================================================================================================
 
-	if ((!doesElementExist(tableContainer))) {
-		tableContainer = document.createElement("div");
-		tableContainer.setAttribute("id", "table-container");
-		body.appendChild(tableContainer);
-	}// if
+	function formatNumber(num) {
 
-	else {
-		clearTable();
-	}// else
+		const formatter = 
+			new Intl.NumberFormat("en-US");
 
-	const table = document.createElement("table");
-	const tableHeader = document.createElement("thead");
-	const tableBody = document.createElement("tbody");
+		const formattedNumber = 
+			formatter.format(num);
 
-	table.appendChild(tableHeader);
-	table.appendChild(tableBody);
+		return formattedNumber;
 
-	tableContainer.appendChild(table);
+	} //formatNumber()
 
-	table.style.visibility = "visible";
-
-	const headerRow = document.createElement("tr");
-
-
-	const bookIDHeaderCell = document.createElement("th");
-	bookIDHeaderCell.textContent = "ID";
-	// headerRow.appendChild(bookIDHeaderCell);
-	bookIDHeaderCell.style.visibility = "hidden";
-
-	const titleHeaderCell = document.createElement("th");
-	titleHeaderCell.textContent = "Title";
-	headerRow.appendChild(titleHeaderCell);
-
-	const authorHeaderCell = document.createElement("th");
-	authorHeaderCell.textContent = "Author";
-	headerRow.appendChild(authorHeaderCell);
-
-	const pagesHeaderCell = document.createElement("th");
-	pagesHeaderCell.textContent = "Number of Pages";
-	headerRow.appendChild(pagesHeaderCell);
-
-	const statusHeaderCell = document.createElement("th");
-	statusHeaderCell.textContent = "Has the book been read?";
-	headerRow.appendChild(statusHeaderCell);
-
-	tableHeader.appendChild(headerRow);
-
-	const btnsContainer = document.createElement("div");
-	btnsContainer.setAttribute("class", "btns-container");
-	tableContainer.appendChild(btnsContainer);
-
-
-	library.forEach((book => {
-
-		const contentRow = document.createElement("tr");
-
-		const bookIDCell = document.createElement("td");
-		bookIDCell.textContent = `${book.id}`;
-		// contentRow.appendChild(bookIDCell);
-		bookIDCell.style.visibility = "hidden";
-
-		const titleCell = document.createElement("td");
-		titleCell.textContent = book.title;
-		contentRow.appendChild(titleCell);
-
-		const authorCell = document.createElement("td");
-		authorCell.textContent = book.author;
-		contentRow.appendChild(authorCell);
-
-		const pagesCell = document.createElement("td");
-		pagesCell.textContent = `${book.pages}`;
-		contentRow.appendChild(pagesCell);
-
-		const statusCell = document.createElement("td");
-		statusCell.textContent = book.read;
-		contentRow.appendChild(statusCell);
-
-		tableBody.appendChild(contentRow);
-
-		const div = document.createElement("div");
-		div.setAttribute("id", "edit-btns-container");
-
-		const deleteBtn = document.createElement("button");
-		deleteBtn.setAttribute("class", "remove-btn");
-
-		deleteBtn.setAttribute("data-bookID", `${book.id}`);
-
-		const deleteIcon = document.createElement("img");
-		let path = "../images/delete.png";
-		deleteIcon.setAttribute("src", path);
-		deleteIcon.setAttribute("class", "remove-icon")
-
-		deleteBtn.appendChild(deleteIcon);
-		div.appendChild(deleteBtn);
-
-		const changeStatusBtn = document.createElement("button");
-		changeStatusBtn.setAttribute("data-bookID", `${book.id}`);
-		changeStatusBtn.textContent = "Change Status";
-		div.appendChild(changeStatusBtn);
-
-		btnsContainer.appendChild(div);
-
-		clearInputs();
-		removeBookFromLibrary(deleteBtn, contentRow);
-		toggleBookStatus(changeStatusBtn, statusCell, book.read);
-	}));
-
-}// displayLibrary()
-
-//=======================================================================================================
-
-function removeBookFromLibrary(button, row) {
-
-	button.addEventListener("click", () => {
-
-		row.remove(); // remove the book from the display
-
-		let index = Number(button.getAttribute("data-bookID"));
-		delete library[index]; // remove the book from the 'library' array
-
-		library.length--;
-	});
-}// removeBookFromLibrary()
-
-//=======================================================================================================
-
-function clearTable() {
-	const tableContainer = document.querySelector("#table-container");
-	tableContainer.innerHTML = "";
-}// clearTable()
-
-//=======================================================================================================
-
-function toggleBookStatus(button, cell, status) {
-
-	button.addEventListener("click", () => {
-
-		//Change the value of the book's read status
-		switch ((status)) {
-			case true:
-				status = false;
-				break;
-			case false:
-				status = true;
-				break;
-			default:
-				break;
-		}// switch()
-
-		let index = Number(button.getAttribute("data-bookID"));
-		library[index].read = status
-
-		cell.textContent = status;
-	});
-}// toggleBookStatus();
-
-//=======================================================================================================
-
-
-function doesElementExist(element) {
-	return ((element != null));
-}// doesElementExist()
-
-//=======================================================================================================
-
-function formatNumber(num) {
-	let formatter = new Intl.NumberFormat("en-US");
-	let formattedNumber = formatter.format(num);
-	return formattedNumber;
-}//formatNumber()
+	//=======================================================================================================
